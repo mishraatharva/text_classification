11703ebb2a7fa0fbe9299e58a40c255d4a5b698f
mishraatharva <mishraatharva825@gmail.com>
2024-03-21 19:02:41 +0530

    data added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # text_classification
+
+```bash
+conda create -n env python=3.8 -y
+```
+
+```bash
+conda activate env
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="atharva",
+    author_email="mishratharva825@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
