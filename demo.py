1a2a08fb09642526e21eb35746b11b1de4a09af6
mishraatharva <mishraatharva825@gmail.com>
2024-03-23 17:13:18 +0530

    Initial Cloud Setup Completed

--- demo.py
+++ demo.py
@@ -1,2 +1,6 @@
-from hate.logger import logging
-logging.info("welcome to out Project")
+# from hate.logger import logging
+# logging.info("welcome to out Project")
+
+from hate.configuration.gcloud_syncer import GCloudSync
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate-speech-bucket007", "dataset.zip", "data/dataset.zip")
